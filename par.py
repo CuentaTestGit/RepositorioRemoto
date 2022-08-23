d88f7ff19c70bc854cf1f2b72e41999479929242
Gustavo Guzman <cuentatestgit@gmail.com>
2022-08-22 21:18:48 -0300

    Agrego función par y los tests correspondientes

---
+++ par.py
@@ -0,0 +1,5 @@
+def par(numero: int) -> bool:
+    """Retorna True si el número ingresado por parámetro es par.
+    De lo contrario, retorna False."""
+    if numero%2 == 0:
+        return True
---
+++ test.py
@@ -0,0 +1,5 @@
+from par import par
+
+print("Número ingresado: 2", par(2))
+print("Número ingresado: 1980", par(1980))
+print("Número ingresado: 1", par(1))
